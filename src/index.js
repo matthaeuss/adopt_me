0e8bcece62f4367b1fec8e209665f67fe93ce798
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-04-20 20:13:02 +0200

    react and reactDOM latest version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-/* global React ReactDOM */
+import React from "react";
+import * as ReactDOM from "react-dom/client";
 
 const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
+    React.createElement("h2", { key: 1 }, props.name),
+    React.createElement("h3", { key: 2 }, props.animal),
+    React.createElement("h3", { key: 3 }, props.breed),
   ]);
 };
 
 const App = () => {
-  return React.createElement("div", {}, [
+  return React.createElement(
+    "div",
+    {},
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
       name: "Luna",
@@ -20,8 +23,8 @@ const App = () => {
       name: "Pepper",
       animal: "Bird",
       breed: "Cockatiel",
-    }),
-  ]);
+    })
+  );
 };
 
 const container = document.getElementById("app");
