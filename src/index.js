efe35b5b07560f803d4b7029631e68597bff24e1
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-04-20 19:35:54 +0200

    add prettier to project

--- src/index.js
+++ src/index.js
@@ -1,31 +1,26 @@
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-       React.createElement("h2", {}, props.name),
-       React.createElement("h3", {}, props.animal),
-       React.createElement("h3", {}, props.breed),
-    ]);
-}
-
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Luna",
-                animal: "Dog",
-                breed: "Havanese"
-            }),
-            React.createElement(Pet, {
-                name: "Pepper",
-                animal: "Bird",
-                breed: "Cockatiel"
-            }),
-        ]
-    );
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+  ]);
+};
 
 const container = document.getElementById("app");
 const root = ReactDOM.createRoot(container);
